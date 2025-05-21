7ecdf1a2eeaaeab878569798e3b68c6df397638d
OzoDev <forpremiummine@gmail.com>
2025-05-21 21:45:50 +0500

    i wanna be yoouurs��

--- JS/products.js
+++ JS/products.js
@@ -1,7 +1,7 @@
-let productWrapper1 = document.querySelector('.products2__wrapper');
-let productWrapper2 = document.querySelector(".bottom-products");
-let productWrapper3 = document.querySelector(".greatfor-cultular");
-let productWrapper4 = document.querySelector(".greatfor-bars");
+let productWrapper1 = document.querySelector('.products__wrapper2');
+let productWrapper2 = document.querySelector(".products__wrapper3");
+let productWrapper3 = document.querySelector(".products__wrapper4");
+let productWrapper4 = document.querySelector(".comments");
 let productWrapper5 = document.querySelector(".greatfor-archtours");
 let productWrapper6 = document.querySelector(".greatfor-eatdrink");
 for(let i = 0; i < data.length; i++){
@@ -9,58 +9,49 @@ for(let i = 0; i < data.length; i++){
                 <li class="product">
                     <img src="${data[i].img}" alt="" class="product-img">
                     <div class="product__titles">
-                        <h3 class="product-title">${data[i].name}</h3>
+                        <h3 class="product-title2">${data[i].name}</h3>
                         <img src="" alt="" class="product-rating">
                         <p class="product-subtitle">${data[i].description}</p>
                     </div>
                 </li>
         `)
-        productWrapper2.insertAdjacentHTML('beforeend',`
-                    <li class="bottom-product">
-                        <img src="${data[i].img}" alt="" class="bottom-img">
-                        <p class="bp-title">${data[i].name}</p>
-                        <p class="bp-subtitle">${data[i].description}</p>
-                    </li>
+}
+for(let i = 0; i < data.length; i++){
+    if(data[i].category == "restaurant"){
+    productWrapper2.insertAdjacentHTML('beforeend',`
+                <li class="product">
+                    <img src="${data[i].img}" alt="" class="product-img">
+                    <div class="product__titles">
+                        <h3 class="product-title2">${data[i].name}</h3>
+                        <img src="" alt="" class="product-rating">
+                        <p class="product-subtitle">${data[i].description}</p>
+                    </div>
+                </li>
         `)
-        productWrapper3.insertAdjacentHTML('beforeend',`
-                    <li class="greatfor-tour">
-                        <img src="${data[i].img}" alt="" class="tour-img">
-                        <div class="tour-titles">
-                            <h4 class="tour-title">${data[i].name}</h4>
-                                <img src="" alt="" class="tour-rating">
-                                <p class="tour-price">from $189 per adult</p>
-                        </div>
-                    </li>
+    }
+}
+for(let i = 0; i < data.length; i++){
+    if(data[i].category == "arts" || data[i].category == "museums" ){
+    productWrapper3.insertAdjacentHTML('beforeend',`
+                <li class="product">
+                    <img src="${data[i].img}" alt="" class="product-img">
+                    <div class="product__titles">
+                        <h3 class="product-title2">${data[i].name}</h3>
+                        <img src="" alt="" class="product-rating">
+                        <p class="product-subtitle">${data[i].description}</p>
+                    </div>
+                </li>
         `)
-        productWrapper4.insertAdjacentHTML('beforeend',`
-                    <li class="greatfor-tour">
-                        <img src="${data[i].img}" alt="" class="tour-img">
-                        <div class="tour-titles">
-                            <h4 class="tour-title">${data[i].name}</h4>
-                                <img src="" alt="" class="tour-rating">
-                                <p class="tour-price">from $189 per adult</p>
-                        </div>
-                    </li>
-        `)
-        productWrapper5.insertAdjacentHTML('beforeend',`
-                    <li class="greatfor-tour">
-                        <img src="${data[i].img}" alt="" class="tour-img">
-                        <div class="tour-titles">
-                            <h4 class="tour-title">${data[i].name}</h4>
-                                <img src="" alt="" class="tour-rating">
-                                <p class="tour-price">from $189 per adult</p>
-                        </div>
-                    </li>
-        `)
-        productWrapper6.insertAdjacentHTML('beforeend',`
-                    <li class="greatfor-tour">
-                        <img src="${data[i].img}" alt="" class="tour-img">
-                        <div class="tour-titles">
-                            <h4 class="tour-title">${data[i].name}</h4>
-                                <img src="" alt="" class="tour-rating">
-                                <p class="tour-price">from $189 per adult</p>
-                        </div>
-                    </li>
+    }
+}
+for(let i = 0; i < data.length; i++){
+    productWrapper4.insertAdjacentHTML('beforeend',`
+                <li class="comment">
+                    <p class="comment-title">${data[i].comment.text}</p>
+                    <div class="coment">
+                    <img src="./assets/https_/www.tripadvisor.com/Tourism-g293968-Tashkent_Tashkent_Province-Vacations.html by html.to.design ❤️ FREE version - 19/05/SVG.svg" alt="" class="comment-avatar">
+                    <p class="coment-coment">By ${data[i].comment.name} • 12 replies</p>
+                    </div>
+                </li>
         `)
 }
-
